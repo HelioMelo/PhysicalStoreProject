9ceed52f4243a142851805673beefe2c78a0f221
HelioMelo <Helio_melo_@hotmail.com>
2024-10-23 16:31:01 -0300

    feat: implement findNearbyStores method in StoreService

--- src/services/storeService.ts
+++ src/services/storeService.ts
@@ -2,8 +2,14 @@ import { zipCodeApi } from "../api/zipCodeApi";
 import { geocodeAddressApi } from "../api/geocodeAddressApi";
 import { Store } from "../models/store";
 import { StoreDTO } from "../dto/storeDTO";
-import { saveStore, checkStoreExists } from "../repository/storeRepository";
+import {
+  saveStore,
+  checkStoreExists,
+  getAllStores,
+} from "../repository/storeRepository";
 import { validateStoreDTO, handleError } from "../errors/errorHandlers";
+import { AddressService } from "./AddressService";
+import { DistanceCalculator } from "../utils/DistanceCalculator";
 
 export class StoreService {
   static async saveStore(storeDTO: StoreDTO) {
@@ -40,6 +46,37 @@ export class StoreService {
       return StoreDTO.fromStore(store);
     } catch (error) {
       const errorMessage = handleError(error, "Erro ao salvar loja");
+      throw new Error(errorMessage);
+    }
+  }
+  static async findNearbyStores(
+    zipCode: string,
+    number?: string
+  ): Promise<StoreDTO[]> {
+    try {
+      validateStoreDTO({ name: "dummy", zipCode });
+
+      const fullAddress = await AddressService.getFullAddress(zipCode, number);
+      const coordinates = await AddressService.getCoordinates(fullAddress);
+
+      const userLat = parseFloat(coordinates.latitude);
+      const userLon = parseFloat(coordinates.longitude);
+
+      const stores = await getAllStores();
+
+      const nearbyStores = DistanceCalculator.filterNearbyStores(
+        stores,
+        userLat,
+        userLon,
+        100
+      );
+      if (nearbyStores.length === 0) {
+        throw new Error("Não há lojas dentro de um raio de 100 km.");
+      }
+
+      return nearbyStores;
+    } catch (error) {
+      const errorMessage = handleError(error, "Erro ao buscar lojas próximas");
       throw new Error(errorMessage);
     }
   }
